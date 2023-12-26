91b318f0ebac98581a45b6af8ef2196af9b9505e
jinvicky <jinvicky17@gmail.com>
2023-12-26 13:01:52 +0900

    Feat: axios get 메서드 작성, 리스트 구현 개선

--- src/apis/Request.ts
+++ src/apis/Request.ts
@@ -1,10 +1,34 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosResponse } from "axios";
 
-export const RequestGet = async (url: string): Promise<AxiosResponse> => {
-    try {
-        const response: AxiosResponse = await axios.get('http://localhost:8080' + url);
-        return response;
-    } catch (error) {
-        throw error; // 오류를 다시 throw하여 상위 레벨에서 처리할 수 있도록 함
-    }
+function convertToQueryString(searchParams: Object) {
+  // 검색 조건이 빈 객체일 경우 빈 문자열 반환
+  if (Object.keys(searchParams).length === 0) {
+    return "";
+  }
+
+  // key-value 쌍을 배열로 매핑하고 URL 쿼리 문자열로 조합
+  const queryString = Object.entries(searchParams)
+    .map(
+      ([key, value]) =>
+        `${encodeURIComponent(key)}=${encodeURIComponent(value)}`
+    )
+    .join("&");
+
+  return `?${queryString}`;
+}
+
+export const RequestGet = async (
+  url: string,
+  paramObj?: any
+): Promise<AxiosResponse> => {
+  let paramStr = "";
+  if (paramObj) paramStr = convertToQueryString(paramObj);
+  try {
+    const response: AxiosResponse = await axios.get(
+      "http://localhost:8080" + url + paramStr
+    );
+    return response;
+  } catch (error) {
+    throw error; // 오류를 다시 throw하여 상위 레벨에서 처리할 수 있도록 함
+  }
 };
--- src/pages/BlogList.tsx
+++ src/pages/BlogList.tsx
@@ -1,51 +1,82 @@
-import { useEffect, useState } from 'react';
-import { RequestGet } from '../apis/Request';
-import './BlogList.scss';
-import Footer from '../common/Footer';
+import { useEffect, useState } from "react";
+import { RequestGet } from "../apis/Request";
+import "./BlogList.scss";
+import Footer from "../common/Footer";
 
 interface Post {
-    id: number;
-    title: string;
-    content: string;
-    img_URL: string;
-    tp_CD: string; //커미션 종류
-    bg_YN: string; //배경 유무
-    rgtr_DT: string; //등록 날짜
+  id: number;
+  title: string;
+  content: string;
+  imgUrl: string;
+  type: string; //커미션 종류
+  isNotBg: string; //배경 유무
+  regDate: string; //등록 날짜
 }
 
 const BlogList = () => {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
+  const [currentTab, setCurrentTab] = useState("TY01");
 
-    useEffect(() => {
-        RequestGet("/post/list").then(resp => {
-            if (resp.data) setData(resp.data)
-        });
+  useEffect(() => {
+    fetchPostList("TY01");
+  }, []);
 
-    }, []);
+  const fetchPostList = (type: string) => {
+    RequestGet("/post/list", { type: type }).then((resp) => {
+      if (resp.data) setData(resp.data);
+    });
+  };
 
-    const renderList = () => {
-        return data.map((post: Post, idx) => {
-            return <div key={idx} className="post-item">
-                <a href={"posts?id=" + post.id}>
-                    <img src={post.img_URL} />
-                </a>
-                <div className="title">{post.title}</div>
-                <div className="content">{post.content}</div>
-
-            </div>;
-        })
-    };
-    return <>
-    <div className="gallery">
-        <div className="header">
-            COMMISSIONS
+  const renderList = () => {
+    return data.map((post: Post, idx) => {
+      return (
+        <div key={idx} className="post-item">
+          <img src={post.imgUrl} />
+          <div className="title">{post.title}</div>
+          <div className="content">{post.content}</div>
         </div>
-        <div className="works">
-            {renderList()}
+      );
+    });
+  };
+
+  return (
+    <>
+      <div className="gallery">
+        <div className="header">COMMISSIONS</div>
+        <div className="tabList">
+          <div
+            className={"tab" + (currentTab === "TY01" ? " active" : "")}
+            onClick={() => {
+              fetchPostList("TY01");
+              setCurrentTab("TY01");
+            }}
+          >
+            {"1인"}
+          </div>
+          <div
+            className={"tab" + (currentTab === "TY03" ? " active" : "")}
+            onClick={() => {
+              fetchPostList("TY03");
+              setCurrentTab("TY03");
+            }}
+          >
+            {"2인"}
+          </div>
+          <div
+            className={"tab" + (currentTab === "TY05" ? " active" : "")}
+            onClick={() => {
+              fetchPostList("TY05");
+              setCurrentTab("TY05");
+            }}
+          >
+            {"단체"}
+          </div>
         </div>
-    </div>
-    <Footer />
-    </>;
-}
+        <div className="works">{renderList()}</div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
 export default BlogList;
