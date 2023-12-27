df06f869c8eb255fae4b0b7c0781e3ddc7e8831a
jinvicky <jinvicky17@gmail.com>
2023-12-27 23:31:05 +0900

    Feat: index component 추가

--- src/common/Router.tsx
+++ src/common/Router.tsx
@@ -1,16 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import BlogList from '../pages/blog/BlogList';
-import CmsList from '../pages/cmsList/CmsList';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import BlogList from "../pages/blog/BlogList";
+import CmsList from "../pages/cmsList/CmsList";
+import MainPage from "../pages/main/MainPage";
 
 const Router = () => {
-    return (
-      <BrowserRouter>
-          <Routes>
-            <Route path="/posts" element={<BlogList />} />
-            <Route path="/commissions" element={<CmsList />} />
-          </Routes>
-      </BrowserRouter>
-    );
-  };
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/posts" element={<BlogList />} />
+        <Route path="/commissions" element={<CmsList />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-  export default Router;
+export default Router;
---
+++ src/pages/main/MainPage.tsx
@@ -0,0 +1,17 @@
+import "./style.scss";
+
+const MainPage = () => {
+  return (
+    <>
+      <div className="main-wrapper">
+        <div>안녕하세요. SD 일러스트를 좋아하는 개발자 걍진입니다.</div>
+        <div>
+          현재 커미션 나라 카페, 트위터, 인스타 등을 통해서 SD 일러스트 작업을
+          하고 있습니다.
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MainPage;
