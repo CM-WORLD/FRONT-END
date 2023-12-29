259d1281c6f0aaafd6057db18884ee3cec983873
jinvicky <jinvicky17@gmail.com>
2023-12-29 10:30:19 +0900

    Feat: 인덱스 페이지 소개 텍스트 작업

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -6,10 +6,13 @@ const MainPage = () => {
     <>
       <div className="main-wrapper">
         <SlideShow />
-        <div>안녕하세요. SD 일러스트를 좋아하는 개발자 걍진입니다.</div>
-        <div>
-          현재 커미션 나라 카페, 트위터, 인스타 등을 통해서 SD 일러스트 작업을
-          하고 있습니다.
+        <h1 className="info-header">INFORMATION</h1>
+        <div className="center-text">
+          <div>안녕하세요. SD 일러스트를 그리는 개발자 걍진입니다.</div>
+          <div>
+            현재 카페, 트위터, 인스타 등을 통해서 SD 일러스트 작업을 하고
+            있습니다.
+          </div>
         </div>
       </div>
     </>
