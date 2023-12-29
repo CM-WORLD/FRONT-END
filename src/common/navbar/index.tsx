21c69f3b6d761caf885ac2f71c2ba288efc6f3a4
jinvicky <jinvicky17@gmail.com>
2023-12-29 20:53:26 +0900

    Feat: navBar에 인증 링크 추가

--- src/common/navbar/index.tsx
+++ src/common/navbar/index.tsx
@@ -5,6 +5,16 @@ const NavBar = () => {
   return (
     <>
       <nav className="nav">
+        <div className="nav-auth">
+          <ul>
+            <li>
+              <a href="/mypage">마이페이지</a>
+            </li>
+            <li>
+              <a href="/signin">로그인</a>
+            </li>
+          </ul>
+        </div>
         <ul>
           <li>
             <a href="/posts">Illust</a>
