d323001d79f9430317b265099ca53206693e6e76
jinvicky <jinvicky17@gmail.com>
2024-01-01 21:26:06 +0900

    Fix: Modal 컴포넌트 수정

--- src/common/modal/index.tsx
+++ src/common/modal/index.tsx
@@ -5,8 +5,6 @@ interface ModalProps {
   title?: string;
   content: ReactNode;
   display: boolean;
-  onClick: () => void;
-  onClose?: () => void;
 }
 
 const Modal = (props: ModalProps) => {
@@ -16,11 +14,8 @@ const Modal = (props: ModalProps) => {
       <div className="modal-overlay"></div>
       <div className="modal-wrapper">
         <div className="modal-content">
-          <div className="title">{props.title}</div>
+          <h2 className="title">{props.title}</h2>
           <div className="content">{props.content}</div>
-          {/* <div className="btn-box">
-            <button>고민중</button>
-          </div> */}
         </div>
       </div>
     </>
