3ffae5cbc7ce57bde7a9770d31b8af5f7cbb142e
jinvicky <jinvicky17@gmail.com>
2024-01-04 12:25:39 +0900

    Feat: 신청완료측 링크 버튼

--- src/pages/apply/complete/index.tsx
+++ src/pages/apply/complete/index.tsx
@@ -15,6 +15,10 @@ const CmsApplyComplete = (props: ApplyCompleteProps) => {
             <p>*ID는 페이지를 나가면 다시 볼 수 없으니 기록해 주세요.</p>
             <p>*비회원은 커미션 ID를 통해서만 커미션 상태를 조회할 수 있습니다.</p>
         </div>
+
+        <div className="link-box">
+            <a href="/">메인으로 이동</a>
+        </div>
     </div>
     </>;
 }
