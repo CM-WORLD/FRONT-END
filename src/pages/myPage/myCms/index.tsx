4b0afa40388718ed1bf10aa14d7cace7e851b308
jinvicky <jinvicky17@gmail.com>
2024-01-05 14:35:25 +0900

    Feat: ui 추가

--- src/pages/myPage/myCms/index.tsx
+++ src/pages/myPage/myCms/index.tsx
@@ -10,35 +10,53 @@ const MyCmsList = () => {
   const [data, setData] = useState([]);
 
   const params = {
-    page: 0, 
-    size: 10
-  }
+    page: 0,
+    size: 10,
+  };
   /* 세션과 회원 때문에 추후 개발 */
   useEffect(() => {
-    API.get("/apply/list/" + "unknown_baf18dad", {params}).then((resp) => {
+    API.get("/apply/list/" + "unknown_baf18dad", { params }).then((resp) => {
       if (resp.data.status === 200) {
         const cmsList = resp.data.data.content;
         setData(cmsList);
       }
+
+      
     });
   }, []);
 
   const cmsApplyList = () => {
-    if(data.length < 1) return <>현재 신청한 내역이 없습니다.</>
+    if (data.length < 1) return <>현재 신청한 내역이 없습니다.</>;
 
-    return data.map((item: CmsApplyDetail, idx)=> {
-      return <tr key={`my-cms-apply-${idx}`}>
-      <td>
-        <a href={`/mypage/cms/${item.id}`}>
-          {item.id}
-        </a>
-      </td>
-      <td className="contents">{item.title}</td>
-      <td>{item.status}</td>
-      <td>{item.regDate}</td>
-    </tr>
-    })
-  }
+    return data.map((item: CmsApplyDetail, idx) => {
+      return (
+        <tr key={`my-cms-apply-${idx}`}>
+          <td>
+            <a href={`/mypage/cms/${item.id}`}>{item.id}</a>
+          </td>
+          <td className="contents">{item.title}</td>
+          <td>
+            <span>{item.status}</span>
+            {/* TODO:: 분기처리 */}
+            <div className="status-btn-box">
+              <a href="/payment" className="pay-link">
+                결제
+              </a>
+              <a
+                className="rvw-link"
+                onClick={(e) => {
+                  e.preventDefault();
+                }}
+              >
+                리뷰 작성
+              </a>
+            </div>
+          </td>
+          <td>{item.regDate}</td>
+        </tr>
+      );
+    });
+  };
   const content = (
     <>
       <div className="my-cms-history">
@@ -46,7 +64,7 @@ const MyCmsList = () => {
           <colgroup>
             <col width="20%" />
             <col width="*" />
-            <col width="15%" />
+            <col width="30%" />
             <col width="15%" />
           </colgroup>
           <thead>
@@ -57,9 +75,7 @@ const MyCmsList = () => {
               <th scope="col">등록 날짜</th>
             </tr>
           </thead>
-          <tbody>
-          {cmsApplyList()}
-          </tbody>
+          <tbody>{cmsApplyList()}</tbody>
         </table>
       </div>
     </>
