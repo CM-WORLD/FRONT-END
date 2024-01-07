18fce1e76ce8350310e26316d1e8dcbef145e7c7
jinvicky <jinvicky17@gmail.com>
2024-01-07 20:34:47 +0900

    .env에 앱 정보 분리

--- src/pages/signIn/page/index.tsx
+++ src/pages/signIn/page/index.tsx
@@ -4,24 +4,22 @@ const SignInPage = () => {
   return (
     <>
       <div className="sign-in-base">
-        {/* <div className="sign-in-wrapper">
-          <h1>비회원 조회</h1>
-          <div className="search-cms">
-            <input
-              className="input"
-              type="text"
-              placeholder="커미션 번호를 입력해 주세요"
-            />
-            <button className="search-btn">조회</button>
-          </div>
-          <p>*해당 커미션 1건에 대해서만 조회가 가능합니다.</p>
-        </div> */}
         <div className="sign-in-wrapper">
           <h1>로그인</h1>
 
           <div className="btn-form">
             <button className="naver">네이버로 로그인</button>
-            <button className="kakao">카카오로 로그인</button>
+            <button
+              className="kakao"
+              onClick={(e) => {
+                const clientId = process.env.REACT_APP_KAKAO_CLIENT_ID;
+                const redUrl = process.env.REACT_APP_KAKAO_REDIRECT_URL_LOCAL;
+                const url = `https://kauth.kakao.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redUrl}&response_type=code&scope=talk_message&email`;
+                window.location.href = url;
+              }}
+            >
+              카카오로 로그인
+            </button>
           </div>
         </div>
       </div>
