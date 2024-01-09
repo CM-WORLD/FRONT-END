14b1039ac3f3dac479c0c21f8e3986bb6dba81c8
jinvicky <jinvicky17@gmail.com>
2024-01-09 13:26:27 +0900

    Feat: authInterceptor 추가

--- src/common/Request.ts
+++ src/common/Request.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import qs from 'qs'
+import qs from "qs";
 
 export const HOST_URL = "http://localhost:8080";
 
@@ -9,37 +9,52 @@ export default axios.defaults.paramsSerializer = (params) => {
 
 /** axios 생성 공통 메서드,  */
 export const API_JSON = axios.create({
-  baseURL: "http://localhost:8080", 
+  baseURL: "http://localhost:8080",
   headers: {
-    "Content-Type": "application/json", 
-  }, 
-  withCredentials: true, 
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
 });
 
 export const API = axios.create({
-  baseURL: "http://localhost:8080",  
-  withCredentials: true, 
+  baseURL: "http://localhost:8080",
+  withCredentials: true,
 });
 
 /* 토큰 조회 */
 export const getAtk = () => {
   return localStorage.getItem("atk");
-}
+};
 
 export const getRtk = () => {
   return localStorage.getItem("rtk");
-}
+};
 
 export const getNick = () => {
   return localStorage.getItem("nick");
-}
+};
 
 /* test용 인증 interceptor */
 export const AUTH_ITC = axios.create({
-  baseURL: "http://localhost:8080",  //모든 요청에 처리하면 안되므로 추후 백엔드 인증 ept는 맨 앞에 /auth 추가하기. 
+  baseURL: "http://localhost:8080",
   headers: {
-    withCredentials: true, 
+    withCredentials: true,
     Authorization: `Bearer ${getAtk()}`,
     RefreshToken: getRtk(),
+  },
+});
+
+AUTH_ITC.interceptors.response.use((resp) => {
+  const status = resp.data.status; 
+  if (status === 200) return resp;
+  else if (status === 205) { // atk 재발급
+    if (resp.data.newAtk) {
+      localStorage.setItem("atk", resp.data.newAtk);
+    } 
+  } else { //415, 500, 505
+    localStorage.setItem("referer", window.location.pathname);
+    window.location.href = "/sign/in";
   }
-}).get("/validate/token");
+
+  return resp;
+});
