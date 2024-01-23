92405d2fd356af0bc584b49e07108c516322fca9
jinvicky <jinvicky17@gmail.com>
2024-01-23 18:06:06 +0900

    Fix, Refactor:  리뷰리스트 페이지 리팩토링

--- src/routers/review/page/index.tsx
+++ src/routers/review/page/index.tsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
-import "./style.scss";
+
 import { CmsApplyDetail } from "../../../defines/api";
-import { HOST_URL } from "../../../libs/Const";
+import { ApiClient } from "../../../libs/ApiClient";
 
 interface ReviewItem {
   id: number;
@@ -16,23 +15,48 @@ interface ReviewItem {
 const ReviewPage = () => {
   const [data, setData] = useState([]);
 
+  const [pageObj, setPageObj] = useState({
+    number: 0,
+    first: true,
+    last: true,
+    size: 10,
+    totalPages: 1,
+    totalElements: 1,
+    empty: true,
+  });
+
   useEffect(() => {
-    axios.get(HOST_URL + "/review/list").then((resp) => {
-      if (resp.data.status === 200) {
-        setData(resp.data.data);
+    ApiClient.getInstance().get(
+      "/review/list",
+      { params: { page: pageObj.number, size: pageObj.size } },
+      (data) => {
+        const respData = data.data;
+        if (respData) {
+          setData(respData.content);
+          setPageObj({
+            first: respData.first,
+            last: respData.last,
+            number: respData.number,
+            size: respData.size,
+            totalPages: respData.totalPages,
+            totalElements: respData.totalElements,
+            empty: respData.empty,
+          });
+        }
+      },
+      (data) => {
+        console.log("error", data);
       }
-    });
+    );
   }, []);
 
   const reviewList = () => {
-    if (data.length < 1)
-      return <div className="empty-review">아직 작성된 후기가 없습니다.</div>;
     return data.map((item: ReviewItem, idx) => (
       <>
         <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border border-gray-200 rounded dark:bg-gray-800 dark:border-gray-700">
           <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-              {item.applyDto.cmsDto.name}
+              {/* {item.applyDto.cmsDto.name} */}
             </h3>
             <p className="my-4">{item.content}</p>
           </blockquote>
@@ -53,7 +77,7 @@ const ReviewPage = () => {
         <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border border-gray-200 rounded dark:bg-gray-800 dark:border-gray-700">
           <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-              {item.applyDto.cmsDto.name}
+              {/* {item.applyDto.cmsDto.name} */}
             </h3>
             <p className="my-4">{item.content}</p>
           </blockquote>
@@ -77,36 +101,16 @@ const ReviewPage = () => {
 
   return (
     <>
-      {/* <WriteRvwModal display={true} onClick={() => {}} /> */}
       <div className="review-page h-full">
-        {/* <h1 className="header">커미션 후기</h1> */}
+        <h1 className="m-5 font-bold text-center">커미션 후기</h1>
         <div className="review-content">
-          <div className="grid mb-8 dark:border-gray-700 md:mb-12 md:grid-cols-3 bg-white dark:bg-gray-800 m-10 gap-10">
-            {/* <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-ss-lg md:border-e dark:bg-gray-800 dark:border-gray-700">
-              <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
-                <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                  Very easy this was to integrate
-                </h3>
-                <p className="my-4">
-                  If you care for your time, I hands down would go with this."
-                </p>
-              </blockquote>
-              <figcaption className="flex items-center justify-center ">
-                <img
-                  className="rounded-full w-9 h-9"
-                  src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png"
-                  alt="profile picture"
-                />
-                <div className="space-y-0.5 font-medium dark:text-white text-left rtl:text-right ms-3">
-                  <div>Bonnie Green</div>
-                  <div className="text-sm text-gray-500 dark:text-gray-400 ">
-                    Developer at Open AI
-                  </div>
-                </div>
-              </figcaption>
-            </figure> */}
-            {reviewList()}
-          </div>
+          {data.length > 0 ? (
+            <div className="grid mb-8 dark:border-gray-700 md:mb-12 md:grid-cols-3 bg-white dark:bg-gray-800 m-10 gap-10">
+              {reviewList()}
+            </div>
+          ) : (
+            <div className="m-5 text-center">아직 작성된 후기가 없습니다.</div>
+          )}
         </div>
       </div>
     </>
