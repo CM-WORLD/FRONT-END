95a94e77cece210c817e2616e4ff9e7f2c9ed8b6
jinvicky <jinvicky17@gmail.com>
2024-01-24 17:17:46 +0900

    Feat: 로케일 문의 추가, textarea 개선

--- src/components/textarea/index.tsx
+++ src/components/textarea/index.tsx
@@ -1,16 +1,21 @@
+import { buildClass } from "../../defines/twColors";
 import { getLocaleToString } from "../locale";
 
 interface TextAreaProps {
   placeholder: string;
   value: string;
   onChange: (e) => void;
+  className?: string;
 }
 
 const TextArea = (props: TextAreaProps) => {
   return (
     <textarea
-      className="border border-gray-400 rounded py-2 px-2 resize-none text-base min-h-40"
-      placeholder={getLocaleToString(props.placeholder)}
+      className={buildClass(
+        "border border-gray-400 rounded py-2 px-2 resize-none text-base min-h-40",
+        props.className
+      )}
+      placeholder={props.placeholder}
       value={props.value}
       onChange={props.onChange}
     />
--- src/routers/bbs/common/reply/ReplyForm.tsx
+++ src/routers/bbs/common/reply/ReplyForm.tsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
-import Button from "../../../../components/button";
 import axios from "axios";
+
 import { AUTH_ITC, getAtk, getRtk } from "../../../../libs/request";
 import { HOST_URL } from "../../../../libs/Const";
+
+import Button from "../../../../components/button";
+import Locale from "../../../../components/locale";
+import TextArea from "../../../../components/textarea";
 
 interface ReplyFormProps {
   bbsId: string;
@@ -10,7 +14,7 @@ interface ReplyFormProps {
 
 const ReplyForm = (props: ReplyFormProps) => {
   const [form, setForm] = useState({
-    content: "reply test...",
+    content: "",
     bbsId: props.bbsId,
     parentId: 0,
     parentPath: "",
@@ -23,25 +27,25 @@ const ReplyForm = (props: ReplyFormProps) => {
     formData.append("parentId", String(form.parentId));
     formData.append("parentPath", form.parentPath);
 
-    AUTH_ITC.get(HOST_URL + "/validate/token").then((resp) => {
-      console.log("token check... ", resp.data.status);
-      if (resp.data.status === 200 || resp.data.staus === 205) {
-        axios
-          .post(HOST_URL + "/reply/", formData, {
-            headers: {
-              withCredentials: true,
-              Authorization: `Bearer ${getAtk()}`,
-              RefreshToken: getRtk(),
-            },
-          })
-          .then((resp) => {
-            console.log("post... ", resp);
-          });
-      } else {
-        alert("로그인이 필요합니다.");
-        // 로그인 페이지로 이동
-      }
-    });
+    // AUTH_ITC.get(HOST_URL + "/validate/token").then((resp) => {
+    //   console.log("token check... ", resp.data.status);
+    //   if (resp.data.status === 200 || resp.data.staus === 205) {
+    //     axios
+    //       .post(HOST_URL + "/reply/", formData, {
+    //         headers: {
+    //           withCredentials: true,
+    //           Authorization: `Bearer ${getAtk()}`,
+    //           RefreshToken: getRtk(),
+    //         },
+    //       })
+    //       .then((resp) => {
+    //         console.log("post... ", resp);
+    //       });
+    //   } else {
+    //     alert("로그인이 필요합니다.");
+    //     // 로그인 페이지로 이동
+    //   }
+    // });
   };
 
   return (
@@ -49,17 +53,15 @@ const ReplyForm = (props: ReplyFormProps) => {
       <div className="border-t border-gray-200 py-5">
         <div className="pb-4 font-bold">댓글 작성</div>
         <div className="flex items-center border border-gray-200 rounded p-3">
-          <textarea
-            className="w-full outline-none resize-none"
-            placeholder="댓글을 작성해주세요."
+          <TextArea
+            placeholder={"댓글을 작성해주세요"}
+            className="w-full min-h-20 border-none focus:outline-none"
+            value={form.content}
             onChange={(e) => setForm({ ...form, content: e.target.value })}
-          ></textarea>
-          <Button
-            className="w-20"
-            color="Primary"
-            value="등록"
-            onClick={submitForm}
           />
+          <Button className="w-20" color="Primary" onClick={submitForm}>
+            <Locale k="register" />
+          </Button>
         </div>
       </div>
     </div>
