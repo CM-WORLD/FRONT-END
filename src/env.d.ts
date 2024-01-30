26f52e4e64e871335eb1c6ad50b0eec38b13e5fe
jinvicky <jinvicky17@gmail.com>
2024-01-30 23:42:14 +0900

    Feat: naver login url 적용

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,5 +5,7 @@ interface ImportMeta {
         VITE_REACT_APP_KAKAO_LOGIN_REDIRECT_URL_LOCAL: string;
         VITE_REACT_APP_KAKAO_LOGOUT_URL_LOCAL: string;
         VITE_DANGEROUSLY_DISABLE_HOST_CHECK: boolean;
+        VITE_REACT_APP_NAVER_CLIENT_ID: string;
+        VITE_REACT_APP_NAVER_REDIRECT_URL_LOCAL: string;
     };
 }
--- src/routers/signIn/page/component/index.tsx
+++ src/routers/signIn/page/component/index.tsx
@@ -1,12 +1,17 @@
 import Button from "../../../../components/button";
 import Locale, { getLocaleToString } from "../../../../components/locale";
 import { EAccountType } from "../../../../defines/account";
-import { AssetsRoot } from "../../../../libs/Const";
+import { AssetsRoot, HOST_URL } from "../../../../libs/Const";
 import "./style.scss";
 
 const LoginBtnsComponent = () => {
   const kakaoId = import.meta.env.VITE_REACT_APP_KAKAO_CLIENT_ID;
-  const kakaoRedirectUrl = import.meta.env.VITE_REACT_APP_KAKAO_LOGIN_REDIRECT_URL_LOCAL;
+  const kakaoRedirectUrl = import.meta.env
+    .VITE_REACT_APP_KAKAO_LOGIN_REDIRECT_URL_LOCAL;
+
+  const naverId = import.meta.env.VITE_REACT_APP_NAVER_CLIENT_ID;
+  const naverRedirectUrl = import.meta.env
+    .VITE_REACT_APP_NAVER_REDIRECT_URL_LOCAL;
 
   const loginButtonHandler = (type: EAccountType) => {
     switch (type) {
@@ -15,20 +20,24 @@ const LoginBtnsComponent = () => {
         window.location.href = url;
         return;
       case EAccountType.Twitter:
+        window.location.href = `${HOST_URL}/sign/in/twitter`;
         // TODO:: 트위터 로그인 api 적용
         return;
       case EAccountType.Naver:
         // TODO:: 네이버 로그인 api 적용
+        window.location.href = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${naverId}&redirect_uri=${naverRedirectUrl}`;
         return;
       default:
         // 사용되지않습니다.
         return;
     }
-  }
+  };
 
   return (
     <div className="LoginContainer max-w-md relative m-auto shadow-md p-5 my-16">
-      <h1 className="font-bold text-center mb-4"><Locale k="login" /></h1>
+      <h1 className="font-bold text-center mb-4">
+        <Locale k="twitter_login" />
+      </h1>
       <div className="flex justify-center flex-col gap-3">
         <Button
           className="cursor-pointer bg-kakao "
@@ -41,10 +50,15 @@ const LoginBtnsComponent = () => {
               alt="kakao login"
               onClick={() => loginButtonHandler(EAccountType.Kakao)}
             />
-            <span><Locale k="kakao_login" /></span>
+            <span>
+              <Locale k="kakao_login" />
+            </span>
           </div>
         </Button>
 
+        <Button onClick={() => loginButtonHandler(EAccountType.Naver)}>
+          <Locale k="naver_login" />
+        </Button>
         <Button
           className="cursor-pointer bg-twitter"
           color="Primary"
@@ -57,7 +71,9 @@ const LoginBtnsComponent = () => {
               alt="twitter login"
               onClick={() => loginButtonHandler(EAccountType.Twitter)}
             />
-            <span><Locale k="twitter_login" /></span>
+            <span>
+              <Locale k="twitter_login" />
+            </span>
           </div>
         </Button>
       </div>
