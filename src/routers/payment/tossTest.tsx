b8f56479e28fefea250082596b40e052b398ad42
wkdu0723 <wkdu0723@gmail.com>
2024-02-18 00:40:04 +0900

    Remove: tossTest.tsx 사용하지 않는 소스 제거

--- src/routers/payment/tossTest.tsx
+++ src/routers/payment/tossTest.tsx
@@ -1,16 +1,12 @@
-import { loadTossPayments } from "@tosspayments/payment-sdk";
 import {
   loadPaymentWidget,
   PaymentWidgetInstance,
 } from "@tosspayments/payment-widget-sdk";
 import { nanoid } from "nanoid";
 import { useEffect, useRef } from "react";
-import Button from "../../components/button";
 
-const TossTest = (subject) => {
+const TossTest = () => {
   const clientKey = "test_ck_DnyRpQWGrNqXaKo12WmeVKwv1M9E";
-  const clientSecret = "test_sk_6bJXmgo28ePAmodRWE7jrLAnGKWx"; //절대 올리지 말것
-
   const paymentWidgetRef = useRef<PaymentWidgetInstance | null>(null);
   const price = 50_000;
   const customerKey = nanoid();
@@ -30,26 +26,26 @@ const TossTest = (subject) => {
       <div className="App">
         <h1>주문서</h1>
         <div id="payment-widget" />
-      <button
-      onClick={async () => {
-        const paymentWidget = paymentWidgetRef.current
+        <button
+          onClick={async () => {
+            const paymentWidget = paymentWidgetRef.current
 
-        try {
-          await paymentWidget?.requestPayment({
-          	orderId: nanoid(),
-            orderName: "토스 티셔츠 외 2건",
-            customerName: "김토스",
-            customerEmail: "customer123@gmail.com",
-            successUrl: `http://localhost:3000/toss/success`,
-            failUrl: `${window.location.origin}/fail`,
-        }) 
-        } catch (err) {
-          	console.log(err)
-        }
-     }}
-  >
-  	결제하기
-  </button>
+            try {
+              await paymentWidget?.requestPayment({
+                orderId: nanoid(),
+                orderName: "토스 티셔츠 외 2건",
+                customerName: "김토스",
+                customerEmail: "customer123@gmail.com",
+                successUrl: `http://localhost:3000/toss/success`,
+                failUrl: `${window.location.origin}/fail`,
+              })
+            } catch (err) {
+              console.log(err)
+            }
+          }}
+        >
+          결제하기
+        </button>
       </div>
     </div>
   );
